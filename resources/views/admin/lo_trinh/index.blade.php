09c206bc705dedc4f4ea397be44e01a526a79089
Admin <admin@example.com>
2026-04-14 20:30:50 +0700

    feat: add Leaflet.js and initialize map in modal

--- resources/views/admin/lo_trinh/index.blade.php
+++ resources/views/admin/lo_trinh/index.blade.php
@@ -135,4 +135,42 @@
         </div>
     </div>
 </div>
+
+@push('styles')
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+    crossorigin=""/>
+@endpush
+
+@push('scripts')
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script>
+<script>
+let map = null;
+let markers = [];
+
+// Initialize map when modal is shown
+document.getElementById('routeModal').addEventListener('shown.bs.modal', function() {
+    if (!map) {
+        map = L.map('routeMap').setView([10.762622, 106.660172], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }
+
+    // Force map to recalculate size after modal animation
+    setTimeout(() => map.invalidateSize(), 200);
+});
+
+// Clear map when modal is hidden
+document.getElementById('routeModal').addEventListener('hidden.bs.modal', function() {
+    if (map) {
+        markers.forEach(marker => map.removeLayer(marker));
+        markers = [];
+    }
+});
+</script>
+@endpush
 @endsection
